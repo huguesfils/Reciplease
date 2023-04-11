5d864d4ef4d5db1015d2ffb9e4ac2f67b0461fd6
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2023-04-11 23:11:08 +0200

    initial commit

---
+++ Reciplease/Favorites.swift
@@ -0,0 +1,20 @@
+//
+//  Favorites.swift
+//  Reciplease
+//
+//  Created by Hugues Fils on 11/04/2023.
+//
+
+import SwiftUI
+
+struct Favorites: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Favorites_Previews: PreviewProvider {
+    static var previews: some View {
+        Favorites()
+    }
+}
