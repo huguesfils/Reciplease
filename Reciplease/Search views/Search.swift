71cefcc3c158179bcdf849a67ad23e36cb5361aa
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2023-04-17 21:40:06 +0200

    view improvments add list and textfield

--- Reciplease/Search views/Search.swift
+++ Reciplease/Search views/Search.swift
@@ -8,26 +8,51 @@
 import SwiftUI
 
 struct Search: View {
-    @State private var searchText = ""
+    @State private var ingredients = [String]()
+    @State private var searchInput = ""
     @State private var isNavigate = false
     
     var body: some View {
         NavigationView {
-            VStack(){
-                Text("Ingredient list")
+            VStack(alignment: .center) {
+                TextField("Lemon, cheese, Sausages...", text: $searchInput)
+                    .textFieldStyle(.roundedBorder)
+                    .textInputAutocapitalization(.never)
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
+                
+                List {
+                    ForEach(ingredients, id: \.self) { ingredient in
+                        Text(ingredient)
+                    }.onDelete { indexSet in
+                        ingredients.remove(atOffsets: indexSet)
+                    }
+                }
                 Spacer()
                 NavigationLink(destination: RecipiesList(), isActive: $isNavigate) {
-               
-                Button("Search for recipies") {
-                    self.isNavigate = true
+                        
+                        Button("Search for recipies") {
+                            self.isNavigate = true
+                        }
+                        .buttonStyle(.borderedProminent)
+                        
                 }
-                    .buttonStyle(.borderedProminent)
-                }
-                .searchable(text: $searchText, prompt: "What's in your fridge?")
-                .navigationTitle("Search")
-                .padding(20)
             }
-            .background(.regularMaterial)
+            .padding(20)
+            .toolbar {
+                EditButton()
+            }
+            .navigationTitle("Search")
+            .onSubmit(addIngredient)
+        }
+        
+    }
+    
+    func addIngredient() {
+        let newIngredient = searchInput.lowercased()
+        withAnimation {
+            ingredients.insert(newIngredient, at: 0)
         }
     }
 }
