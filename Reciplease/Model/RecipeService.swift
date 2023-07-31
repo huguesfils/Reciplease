6e85c653ab5710f3a56574ad2aec57c5146a862d
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2023-07-31 15:53:20 +0200

    test bad code response added

--- Reciplease/Model/RecipeService.swift
+++ Reciplease/Model/RecipeService.swift
@@ -20,47 +20,6 @@ class RecipeService {
         self.session = session
     }
     
-    /*
-        func loadData(ingredients: [String], completion: @escaping (Result<[Recipe], Error>) -> Void) {
-            guard let url = URL(string: "https://api.edamam.com/search?q=\(ingredients.joined(separator: ","))&app_id=\(ApiData.api_id)&app_key=\(ApiData.api_key)") else {
-                let info = [NSLocalizedDescriptionKey: "Invalid URL"]
-                let error = NSError(domain: "com.example.app",
-                                    code: -1,
-                                    userInfo: info)
-                completion(.failure(error))
-                return
-            }
-    
-            let task = session.dataTask(with: url) { (data, response, error) in
-    
-                if let error = error {
-                    completion(.failure(error))
-                    return
-    
-                }
-    
-                guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                    completion(.failure())
-                          return
-                        }
-    
-                        // Parse data
-                        if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                            return (decodedResponse.hits ?? [].map { hit in
-                                hit.recipe
-                                completion(Result.success(decodedResponse))
-                            }
-                        } else {
-                                completion(Result.failure(error))
-    
-                      }
-                    }
-    
-            task.resume()
-        }
-    }*/
-    
-    
     func loadData(ingredients: [String]) async throws  -> [Recipe] {
         guard let url = URL(string: "https://api.edamam.com/search?q=\(ingredients.joined(separator: ","))&app_id=\(ApiData.api_id)&app_key=\(ApiData.api_key)")
         else {
@@ -71,7 +30,7 @@ class RecipeService {
         let (data, response) = try await session.data(from: url)
         
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            print("badd code error")
+            print("bad code error")
             throw Error.HttpStatusCodeError
         }
         if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
@@ -84,74 +43,47 @@ class RecipeService {
         }
     }
 }
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
     
+    override class func canInit(with request: URLRequest) -> Bool {
+        // To check if this protocol can handle the given request.
+        return true
+    }
     
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        // Here you return the canonical version of the request but most of the time you pass the orignal one.
+        return request
+    }
     
-    //        func loadData(ingredients: [String]) async throws  -> [Recipe] {
-    //            guard let url = URL(string: "https://api.edamam.com/search?q=\(ingredients.joined(separator: ","))&app_id=\(ApiData.api_id)&app_key=\(ApiData.api_key)")
-    //            else {
-    //                print("Invalid URL")
-    //                return []
-    //            }
-    //            do {
-    //                let (data, _) = try await session.data(from: url)
-    //                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-    //                    return (decodedResponse.hits ?? []).map { hit in
-    //                        hit.recipe
-    //                    }
-    //                } else {
-    //                    return []
-    //                }
-    //            } catch {
-    //                print("Invalid data")
-    //                return []
-    //            }
-    //        }
-    //    }
-    
-    
-        class MockURLProtocol: URLProtocol {
-            static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
-    
-            override class func canInit(with request: URLRequest) -> Bool {
-                // To check if this protocol can handle the given request.
-                return true
-            }
-    
-    
-            override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-                // Here you return the canonical version of the request but most of the time you pass the orignal one.
-                return request
-            }
-    
-            override func startLoading() {
-                guard let handler = MockURLProtocol.requestHandler else {
-                    fatalError("Handler is unavailable.")
-                }
-    
-                do {
-                    // 2. Call handler with received request and capture the tuple of response and data.
-                    let (response, data) = try handler(request)
-    
-                    // 3. Send received response to the client.
-                    client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-    
-                    if let data = data {
-                        // 4. Send received data to the client.
-                        client?.urlProtocol(self, didLoad: data)
-                    }
-    
-                    // 5. Notify request has been finished.
-                    client?.urlProtocolDidFinishLoading(self)
-                } catch {
-                    // 6. Notify received error.
-                    client?.urlProtocol(self, didFailWithError: error)
-                }
-                // This is where you create the mock response as per your test case and send it to the URLProtocolClient.
-            }
-    
-            override func stopLoading() {
-                // This is called if the request gets canceled or completed.
-            }
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("Handler is unavailable.")
         }
+        
+        do {
+            // 2. Call handler with received request and capture the tuple of response and data.
+            let (response, data) = try handler(request)
+            
+            // 3. Send received response to the client.
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            
+            if let data = data {
+                // 4. Send received data to the client.
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            // 5. Notify request has been finished.
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            // 6. Notify received error.
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {
+        // This is called if the request gets canceled or completed.
+    }
+}
 
--- RecipleaseTests/ApiTestCase.swift
+++ RecipleaseTests/ApiTestCase.swift
@@ -10,19 +10,57 @@ import XCTest
 
 final class ApiTestCase: XCTestCase {
     
-    func testVincent() async throws {
+    func testLoadDataWithInvalidData() async throws {
         URLProtocol.registerClass(MockURLProtocol.self)
         MockURLProtocol.requestHandler = {request in
             let encoder = JSONEncoder()
-            let data = try encoder.encode(["toto": "titi"])
+            let data = try encoder.encode(["test": "test"])
             return (HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!, data)
         }
         
         let service = RecipeService()
 
         do {
-            _ = try await service.loadData(ingredients: ["turc"])
+            _ = try await service.loadData(ingredients: ["test"])
             XCTAssert(false)
+        } catch  {
+            XCTAssert(true)
+            XCTAssert(error is RecipeService.Error)
+        }
+    }
+    
+    func testLoadWithBadCodeResponse() async throws {
+        URLProtocol.registerClass(MockURLProtocol.self)
+        MockURLProtocol.requestHandler = {request in
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(["test": "test"])
+            return (HTTPURLResponse(url: request.url!, statusCode: 400, httpVersion: nil, headerFields: nil)!, data)
+        }
+        
+        let service = RecipeService()
+
+        do {
+            _ = try await service.loadData(ingredients: ["test"])
+            XCTAssert(false)
+        } catch  {
+            XCTAssert(true)
+            XCTAssert(error is RecipeService.Error)
+        }
+    }
+    
+    func testLoadWithWithCorrectResponse() async throws {
+        URLProtocol.registerClass(MockURLProtocol.self)
+        MockURLProtocol.requestHandler = {request in
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(["test": "test"])
+            return (HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!, data)
+        }
+        
+        let service = RecipeService()
+
+        do {
+            _ = try await service.loadData(ingredients: ["cheese"])
+            XCTAssert(true)
         } catch  {
             XCTAssert(true)
             XCTAssert(error is RecipeService.Error)
